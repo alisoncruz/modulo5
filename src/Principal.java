1161608c8a5bbbeec64469993b81b0844b57fd31
alisoncruz <alisonn83@gmail.com>
2020-12-09 17:16:54 -0300

    initial commit

---
+++ src/Principal.java
@@ -0,0 +1,17 @@
+public class Principal {
+
+    public static void main (String [] args){
+
+
+        Character characterObj = Character.valueOf('a');
+        Byte byteObj = Byte.valueOf((byte) 1);
+        Short shortObj =  Short.valueOf((short) 15);
+        Integer integerObj = Integer.valueOf((int) 20);
+        Long longObj = Long.valueOf((long) 5);
+        Float floatObj = Float.valueOf((float) 12.5);
+        Double doubleObj = Double.valueOf((double) 22.5);
+        Boolean booleanObj = Boolean.valueOf((boolean) true);
+
+
+    }
+}
